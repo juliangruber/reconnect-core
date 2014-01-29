cc0f4b685ef4196ab721c3c90b4037bdd7865f38
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-01-29 22:15:56 +0100

    be able to connect more then once
    adding the possibility to call `.connect()` without arguments to reuse
    the arguments from the previous call.
    i also made sure that `.connect` can be called more then once to allow
    eg 'upgrading' a connection with tls or whateva. all its needed is a
    additional `.listen` call with the new (cleartext) socket.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var EventEmitter = require('events').EventEmitter
 var backoff = require('backoff')
+var noop = function () {}
 
 module.exports =
 function (createConnection) {
@@ -24,16 +25,18 @@ function (createConnection) {
     })
 
     var args
+    var cleanup = noop
     function attempt (n, delay) {
-      if(emitter.connected) return
       if(!emitter.reconnect) return
 
+      cleanup()
       emitter.emit('reconnect', n, delay)
       var con = createConnection.apply(null, args)
       emitter._connection = con
 
-      function onDisconnect (err) {
-        emitter.connected = false
+      cleanup = onCleanup
+      function onCleanup(err) {
+        cleanup = noop
         con.removeListener('error', onDisconnect)
         con.removeListener('close', onDisconnect)
         con.removeListener('end'  , onDisconnect)
@@ -41,7 +44,13 @@ function (createConnection) {
         //hack to make http not crash.
         //HTTP IS THE WORST PROTOCOL.
         if(con.constructor.name == 'Request')
-          con.on('error', function () {})
+          con.on('error', noop)
+
+      }
+
+      function onDisconnect (err) {
+        emitter.connected = false
+        onCleanup(err)
 
         //emit disconnect before checking reconnect, so user has a chance to decide not to.
         emitter.emit('disconnect', err)
@@ -80,7 +89,6 @@ function (createConnection) {
     emitter.connect =
     emitter.listen = function () {
       this.reconnect = true
-      if(emitter.connected) return
       backoffMethod.reset()
       backoffMethod.on('ready', attempt)
       args = args || [].slice.call(arguments)
